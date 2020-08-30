1af199631f78bcfbd4edc502cec1c6678dadc88c
github username <jesse.ben.berman@gmail.com>
2020-08-30 18:15:01 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { TrucksComponent } from './trucklist/TrucksComponent.component';
+import { MenusComponent } from './menus/menus.component';
+import { RegisterComponent } from './register/register.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'trucks', component: TrucksComponent },
+  { path: 'menus', component: MenusComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'logout', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'reset', component: PasswordResetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/trucklist/TrucksComponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trucklist',
+  templateUrl: './TrucksComponent.component.html',
+  styleUrls: ['./TrucksComponent.component.scss']
+})
+export class TrucksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
